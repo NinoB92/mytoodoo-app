ee9861c8f0f6e654e0dbb811a1425716f720098a
NinoB92 <116513269+NinoB92@users.noreply.github.com>
2023-11-21 15:10:33 +0100

    Vue Router added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './assets/main.css';
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
+import "./assets/main.css";
+import route from "./router";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 
 const app = createApp(App);
 app.use(createPinia());
-app.mount('#app');
-
+app.use(route);
+app.mount("#app");
